f30f48bd379a42c61321ee4bab6a57973d06835b
bbnahihi <buibangnam2006@gmail.com>
2026-04-17 14:48:30 +0700

    Complete assignment 8

---
+++ AimsProject/src/hust/soict/vietphap/aims/media/Book.java
@@ -0,0 +1,63 @@
+package hust.soict.vietphap.aims.media;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+    private int id;
+    private String title;
+    private String category;
+    private float cost;
+    private List<String> authors = new ArrayList<String>();
+
+    public Book() {
+    }
+
+    public void addAuthor(String authorName) {
+        if (!authors.contains(authorName)) {
+            authors.add(authorName);
+        }
+    }
+
+    public void removeAuthor(String authorName) {
+        if (authors.contains(authorName)) {
+            authors.remove(authorName);
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+}
